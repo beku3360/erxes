64375ad65686c0a1f2501cbb1eb39e5868e2b1c9
Dulguun Otgon <dulguun0225@gmail.com>
2024-05-25 03:59:47 +0800

    timeclock

--- packages/plugin-tags-api/src/graphql/resolvers/queries/tags.ts
+++ packages/plugin-tags-api/src/graphql/resolvers/queries/tags.ts
@@ -154,7 +154,7 @@ const tagQueries = {
   /**
    * Get one tag
    */
-  tagDetail(_root, { _id }: { _id: string }, { models }: IContext) {
+  async tagDetail(_root, { _id }: { _id: string }, { models }: IContext) {
     return models.Tags.findOne({ _id });
   },
 };
